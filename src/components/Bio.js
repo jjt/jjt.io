6eb848e4892614fef01da4bd1005952ba60b46e2
Jason Trill <jason@jjt.io>
2018-06-24 08:19:10 -0700

    Tweak bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Bio = () => (
   <div style={{marginBottom: '3rem'}}>
-    <h1>I'm Jason and I ❤ product work</h1>
-    <p>Passionate about where the user hits the screen, I like empowering engineering teams to build great products</p>
+    <h1>Hi, I'm Jason</h1>
+    <p>I like leading and empowering engineering teams to build and ship great products</p>
     <p>Recently I've been Director of Engineering for <a href="https://getflow.com">Flow</a>, a beautiful project management app, and a Lead Engineer at <a href="https://metalab.co">MetaLab</a> building products for clients from startups to Fortune 50 companies</p>
     <p>I can be reached by email at jason @ this domain</p>
   </div>
